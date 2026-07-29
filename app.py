6aa4af4638c74c4b61fc3c720ae5a8a2fe84d079
Sheikh-H <sheikh.hussain1155@gmail.com>
2026-07-29 23:54:09 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, url_for, render_template
+from datetime import timedelta, datetime
+from flask_session import Session
+from dotenv import load_dotenv
 import os
 
+from flask_wtf import CSRFProtect
+
+load_dotenv()
+
 app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.config.update(
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_SAMESITE="LAX",
+    PERMANENT_SESSION_LIFETIME=timedelta(hours=1),
+)
+
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+Session(app)
+
+csrf = CSRFProtect(app)
 
 
 @app.route("/")
 def home_page():
-    return render_template("pages/main/home.html")
+    title = "Online Stock Management System"
+    return render_template("pages/main/home.html", title=title)
 
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 5000)
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
