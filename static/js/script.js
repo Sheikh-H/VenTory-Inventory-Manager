9454b164330e9c60784719df6cc1f1ff50ba81ee
Sheikh-H <sheikh.hussain1155@gmail.com>
2026-07-30 03:58:45 +0100

    working on the home page

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,17 @@
 const menuButton = document.querySelector(".mobile-nav-button");
+const menuMobile = document.querySelector(".navigation-bar");
 menuButton.addEventListener("click", () => {
-  menuButton.classList.toggle("active");
+  const menuOpen = menuButton.classList.toggle("active");
+  menuMobile.classList.toggle("active");
+  if (menuOpen) {
+    document.body.style.overflowY = "hidden";
+  } else {
+    document.body.style.overflowY = "";
+  }
 });
+
+const mobileMenuSpans = document.querySelectorAll(".nav-item-bottom");
+
+mobileMenuSpans.forEach(
+  (item) => (item.style.animationDelay = `${Math.random() * 0.4}s`),
+);
